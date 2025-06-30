2ae6917e9bd6adfd15439ba74ae8fc1bf1e6bcf4
quang.ndt <57524232+quangkyzzz@users.noreply.github.com>
2025-06-30 08:12:56 +0700

    add test widget

--- lib/features/home/exam_feature/get_exam_view.dart
+++ lib/features/home/exam_feature/get_exam_view.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:social_app/common/common.dart';
 
 import 'package:social_app/provider/get_exam_provider.dart';
+import 'package:social_app/theme/pallete.dart';
 
 class GetExamView extends StatefulWidget {
   const GetExamView({super.key});
@@ -29,14 +30,72 @@ class _GetExamViewState extends State<GetExamView> {
       appBar: UIConstants.appBar(),
       body: context.watch<GetExamProvider>().isLoading
           ? Center(child: CircularProgressIndicator())
-          : Column(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.delete, color: Colors.red),
-                  onPressed: () async {},
-                ),
-              ],
+          : ListView.builder(
+              itemCount: context.read<GetExamProvider>().exams.length,
+              itemBuilder: (context, index) {
+                return ExamWidget(
+                  avatar: 'https://i.imgur.com/CbhBdjn.png',
+                  name: _watchProvider().exams[index].examName,
+                  text1: 'text1',
+                  text2: 'text2',
+                );
+              },
             ),
+    );
+  }
+
+  GetExamProvider _readProvider() {
+    return context.read<GetExamProvider>();
+  }
+
+  GetExamProvider _watchProvider() {
+    return context.watch<GetExamProvider>();
+  }
+}
+
+class ExamWidget extends StatelessWidget {
+  final String avatar;
+  final String name;
+  final String text1;
+  final String text2;
+  const ExamWidget({
+    super.key,
+    required this.avatar,
+    required this.name,
+    required this.text1,
+    required this.text2,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {},
+      title: Text(
+        name,
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 10),
+          Text(
+            text1,
+            style: const TextStyle(
+              color: Pallete.whiteColor,
+              fontSize: 16,
+            ),
+          ),
+          Text(
+            text2,
+            style: const TextStyle(
+              fontSize: 15,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
